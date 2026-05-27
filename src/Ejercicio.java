e66f25678b65a83dfcb0b9806a03ab2c6238ac0b
Richard Eduardo Riveros Navas <rriverosnavas@unicartagena.edu.co>
2026-05-27 17:24:50 -0500

    ejercicio 2 de algoritmos

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -1,2 +1,20 @@
-public class Ejercicio {
-}
+ import java.util.Scanner;
+
+
+    public class Ejercicio {
+        public static void main(String[] args) {
+            int y=1;
+            Scanner entrada = new Scanner(System.in);
+
+            int[] arreglo = new int[10];
+
+            for (int i = 0; i <= 9; i++) {
+                System.out.println("Ingrese el numero de la " + y + "ª posicion del vector/arreglo: ");
+                arreglo[i] = entrada.nextInt();
+                y=y+1;
+            }
+            for (int i = 0; i <= 9; i++){
+                System.out.println("El numero de la "+y+"ª posicion del vector/arreglo es: "+arreglo[i]);
+                y=y+1;
+            }}}
+
